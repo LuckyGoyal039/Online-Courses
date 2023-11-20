f1e801d47ad1b7e0cfbf2b6d526415ecdd8919ed
Lucky <goyallucky2020@gmail.com>
2023-11-20 11:45:33 +0530

    complete course listing page and upload course data to firestore firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 import CourseListing from "./pages/CourseListing";
 import StudentDashboard from "./pages/StudentDashboard";
+import CourseDetail from "./pages/CourseDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route>
+          <Route
+            path="/"
+            element={<Navigate to="/course-listing" replace={true} />}
+          />
           <Route path="/course-listing" element={<CourseListing />} />
+          <Route path="/course-detail/:id" element={<CourseDetail />} />
           <Route path="/student-dashboard" element={<StudentDashboard />} />
         </Route>
       </Routes>
